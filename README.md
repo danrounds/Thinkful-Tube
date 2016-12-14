3403db3a03f5361f43db28783d68c07a47964ce4
Dan Rounds <dan.rounds@gmail.com>
2016-12-13 21:05:23 -0500

    ugly draft

---
+++ README.md
@@ -0,0 +1,11 @@
+# A toy search frontend for YouTube
+
+Made in order to test out AJAX with JSON.
+
+Submits a query to YouTube's API and generates a[n incredibly unpolished] search-results page.
+
+To-dos:
+- Use your existing grid stylesheets to make the results look better.
+- You could add < BACK and NEXT > buttons for add'l search results.
+
+-DR, 2016
---
+++ app.js
@@ -0,0 +1,49 @@
+// Stretch:
+// Make the images clickable, playing them in a lightbox
+// Show a link for more from the channel that each video came from
+// Show buttons to get more results (using the previous and next page links from the JSON)
+
+'use strict';
+
+var YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getSearchData(query, callback) {
+    var query = {
+        part: 'snippet',
+        maxResults: 10,
+        key: 'AIzaSyBTNjgDxhK8Valx49hGTSgVJ0wkjCYaqwk',
+        q: query
+    };
+    $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderSearchResults(JSON) {
+    var resultElement = '';
+    if (JSON.items) {
+        JSON.items.forEach(function(item){
+            var link = '<a href="https://www.youtube.com/watch?v='
+                    + item.id.videoId + '" target="_blank">';
+            var pic = '<img src="'+ item.snippet.thumbnails.medium.url
+                +'" alt="search_result_pic"/>';
+
+            var desc = '<p class="vid-desc">'+item.snippet.title+'</p>';
+            var element = '<div class="pic-container">' + link + pic + desc +'</div>';
+            resultElement += element;
+        });
+    } else {
+        resultElement += '<p>No results for your query</p>';
+    }
+    $('section').html(resultElement);
+}
+
+function watchSubmit() {
+    $('.js-search-form').submit(function(e){
+        // var query = $(this).find('.js-query').val();
+        var query = $('.js-query').val();
+        getSearchData(query, renderSearchResults);
+        getSearchData(query, renderSearchResults);
+        getSearchData(query, renderSearchResults);
+    });
+}
+
+$(function(){watchSubmit();});
